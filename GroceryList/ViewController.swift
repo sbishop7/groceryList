ae8c2b62b27d707ff9c0d100649e3cf43f6297bd
Daniel Thompson <danthwa@gmail.com>
2017-07-10 15:22:46 -0700

    initial commit

--- GroceryList/ViewController.swift
+++ GroceryList/ViewController.swift
@@ -8,16 +8,74 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class TableViewController: UITableViewController {
+    
+    var jsonData = [
+        "Produce": ["Celery", "Potato", "Rhubarb"],
+        "Dairy": ["Milk", "Yoghurt"],
+        "Bakery": ["Loaf"]
+    ]
+    
+    struct GroceryCategory {
+        var name: String
+        var items: [String]
+    }
+    
+    var dataArray = [GroceryCategory]()
+    
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+//        var stagingStruct = GroceryCategory(name: "first piece", items: ["blah"])
+        
+        for (key, value) in jsonData {
+            dataArray.append(GroceryCategory(name: key, items: value))
+//            stagingStruct.name = key
+//            stagingStruct.items = value
+//            dataArray.append(stagingStruct)
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return dataArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 100
+    }
+    
+    override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        
+        let view = UIView(frame: CGRect(x: 0, y: 0, width: tableView.frame.size.width, height: 18))
+        
+        let label = UILabel(frame: CGRect(x: 10, y: 5, width: tableView.frame.size.width, height: 18))
+        
+        label.text = dataArray[section].name
+        
+        view.addSubview(label)
+        
+        return view
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return dataArray[section].items.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = dataArray[indexPath.section].items[indexPath.row]
+        return cell
     }
 
 
